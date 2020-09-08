97da518b3e4e0deef944fddc87e1d58da7107576
Christian <christianpavoni1810@gmail.com>
2020-09-08 17:02:01 -0300

    Correcciones importantes conceptuales. Creando un hilo dentro del servicio.

--- app/src/main/java/com/example/serviciomostrarmsjsporconsola/MainActivity.java
+++ app/src/main/java/com/example/serviciomostrarmsjsporconsola/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -23,101 +24,34 @@ import static androidx.core.app.ActivityCompat.requestPermissions;
 import static androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale;
 
 public class MainActivity extends AppCompatActivity {
-    Handler handler = new Handler();
+    private Intent miservicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        validaPermisos();
-        Intent i=new Intent(this,MyService.class);
-        repetirServicio(i);
 
-    }
-
-    private final int TIEMPO = 9000;
-
-    public void repetirServicio(final Intent i) {
-        handler.postDelayed(new Runnable() {
-            public void run() {
-
-                startService(i);
-                handler.postDelayed(this, TIEMPO);
-                stopService(i);
-            }
-
-        }, TIEMPO);
-
-    }
-
-    private boolean validaPermisos() {
-
-        if(Build.VERSION.SDK_INT<Build.VERSION_CODES.M){
-            return true;
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.READ_SMS)
+                != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.READ_SMS},1000);
         }
 
-        if((checkSelfPermission(READ_SMS)== PackageManager.PERMISSION_GRANTED)){
-            return true;
-        }
 
-        if((shouldShowRequestPermissionRationale(READ_SMS))){
-            cargarDialogoRecomendacion();
-        }else{
-            requestPermissions(new String[]{READ_SMS},100);
-        }
-
-        return false;
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if(requestCode==100){
-            if(grantResults.length==2 && grantResults[0]==PackageManager.PERMISSION_GRANTED
-                    && grantResults[1]==PackageManager.PERMISSION_GRANTED){
-                onResume();
-            }else{
-                solicitarPermisosManual();
-            }
-        }
-
+    protected void onResume() {
+        super.onResume();
+        miservicio=new Intent(this,MyService.class);
+        startService(miservicio);
     }
 
-    private void solicitarPermisosManual() {
-        final CharSequence[] opciones={"si","no"};
-        final AlertDialog.Builder alertOpciones=new AlertDialog.Builder(MainActivity.this);
-        alertOpciones.setTitle("¿Desea configurar los permisos de forma manual?");
-        alertOpciones.setItems(opciones, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                if (opciones[i].equals("si")){
-                    Intent intent=new Intent();
-                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                    Uri uri=Uri.fromParts("package",getPackageName(),null);
-                    intent.setData(uri);
-                    startActivity(intent);
-                }else{
-                    Toast.makeText(getApplicationContext(),"Los permisos no fueron aceptados",Toast.LENGTH_SHORT).show();
-                    dialogInterface.dismiss();
-                }
-            }
-        });
-        alertOpciones.show();
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopService(miservicio);
     }
 
-    private void cargarDialogoRecomendacion() {
-        AlertDialog.Builder dialogo=new AlertDialog.Builder(MainActivity.this);
-        dialogo.setTitle("Permisos Desactivados");
-        dialogo.setMessage("Debe aceptar los permisos para el correcto funcionamiento de la App");
-
-        dialogo.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                requestPermissions(new String[]{READ_SMS},100);
-            }
-        });
-        dialogo.show();
-    }
 
 }
--- app/src/main/java/com/example/serviciomostrarmsjsporconsola/MyService.java
+++ app/src/main/java/com/example/serviciomostrarmsjsporconsola/MyService.java
@@ -1,10 +1,14 @@
 package com.example.serviciomostrarmsjsporconsola;
 
+import android.Manifest;
 import android.app.Service;
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
 import android.provider.Telephony;
 import android.util.Log;
@@ -13,35 +17,61 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import static androidx.core.app.ActivityCompat.requestPermissions;
+
 public class MyService extends Service {
     private static int contador=5;
+
     public MyService() {
     }
 
     @Override
-    public void onCreate() {
-        super.onCreate();
-        Uri smsRecibidos= Uri.parse("content://sms/inbox");
-        ContentResolver cr=getContentResolver();
-        Cursor cursor=cr.query(smsRecibidos,null,null,null,null);
+    public int onStartCommand(Intent intent, int flags, int startId) {
 
-        if(cursor.getCount() > 0){
-            while(cursor.moveToNext() && contador>0){
-                String nro=cursor.getString(cursor.getColumnIndex(Telephony.Sms.Inbox.ADDRESS));
-                String texto=cursor.getString(cursor.getColumnIndex(Telephony.Sms.Inbox.BODY));
-                String fecha=cursor.getString(cursor.getColumnIndex(Telephony.Sms.Inbox.DATE));
-                Date fechaMsj= new Date(Long.valueOf(fecha));
 
-                Log.d("salida","De: "+nro);
-                Log.d("salida","Cuerpo: "+texto);
-                Log.d("salida", "Fecha: "+String.valueOf(fechaMsj));
-                Log.d("salida", "----------------------------------------------------");
+        final Uri sms= Telephony.Sms.CONTENT_URI;
+        final ContentResolver cr=getContentResolver();
 
-                contador--;
+
+        Runnable leer=new Runnable() {
+            Cursor cursor;
+            @Override
+            public void run() {
+                while(true) {
+                    cursor = cr.query(sms, null, null, null, null);
+
+                    if (cursor.getCount() > 0) {
+                        while (cursor.moveToNext() && contador > 0) {
+                            String nro = cursor.getString(cursor.getColumnIndex(Telephony.Sms.Inbox.ADDRESS));
+                            String texto = cursor.getString(cursor.getColumnIndex(Telephony.Sms.Inbox.BODY));
+                            String fecha = cursor.getString(cursor.getColumnIndex(Telephony.Sms.Inbox.DATE));
+                            Date fechaMsj = new Date(Long.valueOf(fecha));
+
+                            Log.d("salida", "De: " + nro);
+                            Log.d("salida", "Cuerpo: " + texto);
+                            Log.d("salida", "Fecha: " + String.valueOf(fechaMsj));
+                            Log.d("salida", "----------------------------------------------------");
+
+                            contador--;
+                        }
+                        contador = 5;
+                    }
+                    try{
+                        Thread.sleep(9000);
+                    }
+                    catch (InterruptedException e)
+                    {
+                        Log.e("Error",e.getMessage());
+                    }
+
+                }
             }
-            contador=5;
-        }
+        };
+        Thread thread=new Thread(leer);
+        thread.start();
+        return START_STICKY;
     }
+
 
     @Override
     public IBinder onBind(Intent intent) {
